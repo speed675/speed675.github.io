2b3c05ffc8ef6bbd09c712d33b55926e47ee53ce
Ritban Banerjee <banerjeerit00@gmail.com>
2023-10-10 13:44:05 +0530

    content updated

---
+++ 2nd.py
@@ -0,0 +1,6 @@
+import os
+import openai
+from git import Repo
+from pathlib import Path
+PATH_TO_BOLG_REPO = Path("C:\\Users/AT597VF/OneDrive - EY/Documents/GitHub/speed675.github.io/.git")
+print(PATH_TO_BOLG_REPO.parent)
